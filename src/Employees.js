7d3e049e4a4294b2ab9895cb848b194d8f5093cc
Shabir <hussainshabir484@gmail.com>
2021-11-23 03:36:11 +0530

    learned chaining and use of async await- created table component

--- src/Employees.js
+++ src/Employees.js
@@ -1,16 +1,57 @@
-import './App.css';
-import React from 'react';
-import EmployeeList from './components/EmployeeList';
+import React, { useState, useEffect } from 'react';
+import TableDemo from './components/TableDemo';
 
+function Employees() {
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Employee List',
+                columns: [
+                    {
+                        Header: 'Id',
+                        accessor: 'id',
+                    },
+                    {
+                        Header: 'Name',
+                        accessor: 'name',
+                    },
+                    {
+                        Header: 'Email',
+                        accessor: 'email',
+                    },
+                    {
+                        Header: 'Gender',
+                        accessor: 'gender',
+                    },
+                    {
+                        Header: 'Status',
+                        accessor: 'status',
+                    }
+                ]
+            }
+        ],
+        []
+    )
 
+    const [employees, setEmployees] = useState([])
+    const [errorFetchingApiData, setErrorFetchingApiData] = useState(false)
 
-class Employees extends React.Component {
-
-    // lifecycle methods are those which are invoked when components are rendered
-    render() {
-        return (<><EmployeeList /></>);
-
+    const getEmployeeList = async () => {
+        try {
+            const employeeListArr = await fetch("https://gorest.co.in/public/v1/users")
+            const responseJson = await employeeListArr.json()
+            setEmployees(responseJson.data)
+        } catch (error) {
+            setErrorFetchingApiData(true)
+        }
     }
+
+    useEffect(() => {
+        getEmployeeList()
+    }, [])
+
+    return TableDemo(columns, employees, errorFetchingApiData)
+
 }
 
 export default Employees;
---
+++ src/components/TableDemo.js
@@ -0,0 +1,48 @@
+import { useTable } from 'react-table';
+
+function TableDemo(columns, data, errorFetchingApiData) {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data })
+
+    return (errorFetchingApiData ? (<div>Error occured while fetching API data</div>) :
+        <>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}
+                                    style={{
+                                        borderBottom: 'solid 3px blue',
+                                        background: 'green',
+                                        color: 'white',
+                                        fontWeight: 'bold',
+                                    }}
+                                >{column.render('Header')}</th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default TableDemo;
